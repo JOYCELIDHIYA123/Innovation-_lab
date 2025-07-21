78161f22490bf391c289a67d0c9b2d9799a174a7
JOYCELIDHIYA123 <joycelidhiya123@gmail.com>
2025-07-21 09:30:08 +0530

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,37 @@
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+
+let x = 5;
+x += 2;  
+x *= 3;  
+
+console.log(5 == '5');   
+console.log(5 === '5');  
+console.log(5 != 4);     
+console.log(5 !== 5);    
+console.log(5 > 3);      
+
+
+console.log(true && false);
+console.log(true || false); 
+
+
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No";
+console.log("Can Vote:", canVote);
+
+
+console.log(5 & 1); 
+console.log(5 | 1); 
+console.log(~5);    
+
+
+let i = 10;
+console.log(i++); 
+console.log(++i); 
+console.log(i--);
+console.log(--i); 
