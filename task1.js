d9860504bc47974f178f260c6f0e9e9d7155ce86
JOYCELIDHIYA123 <joycelidhiya123@gmail.com>
2025-07-21 09:26:48 +0530

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,16 @@
+
+var name = "Alice";
+let age = 22;
+const country = "India";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Country:", country);
+
+
+name = "Bob"; 
+age = 23;     
+country = "USA";  
+
+console.log("Updated Name:", name);
+console.log("Updated Age:", age);
